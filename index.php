2f1b3344f6fac01a4719c5ebe92639806bd989ce
sowluss <sowluss@gmail.com>
2018-11-07 01:28:17 +0200

    created php login form conntected to phpMyAdmin, added relevant .php files

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login System</title>
+</head>
+<body>
+
+<form action="includes/signup.inc.php" method="POST">
+<input type="text" name="first" placeholder="First Name">
+<br>
+<input type="text" name="last" placeholder="Last Name">
+<br>
+<input type="text" name="email" placeholder="E-mail">
+<br>
+<input type="text" name="uid" placeholder="Username">
+<br>
+<input type="password" name="pwd" placeholder="Password"> 
+<br>
+<button type="submit" name="submit">Sign Up</button>
+</form>
+
+</body>
+</html>
